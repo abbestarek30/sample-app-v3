fb946da17dda570638d00d66ac1f4315b9acc9ac
abbestarek30 <abbes.tarek@gmail.com>
2022-11-07 08:51:19 +0000

    Committing sample-app-v3 files.

--- sample-app.sh
+++ sample-app.sh
@@ -21,5 +21,5 @@ echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t sampleapp-$version .
-docker run -t -d -p 5050:5050 --name samplerunning$version sampleapp$version
+docker run -t -d -p 5050:5050 --name samplerunning-$version sampleapp-$version
 docker ps -a 
